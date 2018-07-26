92c74ae9901274bf9b3da105632e063e7c99e1bb
Ewa-Stasiak <ewa.stasiak.poczta@gmail.com>
2018-07-26 20:19:34 +0200

    feat: Add a location

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -15,7 +15,13 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 export class MapContainer extends Component {
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
+      <Map
+        google={this.props.google}
+        initialCenter={{
+          lat: 45.798327,
+          lng: 24.125583
+          }}
+        zoom={14}>
 
         <Marker onClick={this.onMarkerClick}
                 name={'Current location'} />
