047dd481bc874f337e397f765881b7c3e8e460e7
Ewa-Stasiak <ewa.stasiak.poczta@gmail.com>
2018-08-01 09:38:08 +0200

    feat: Add buggy search

--- src/SearchList.js
+++ src/SearchList.js
@@ -3,28 +3,92 @@ import App from './App.css';
 // import MarkerList from './MarkerList.js'
 // import Castles from './castles.json';
 import Castle from './Castle.js';
-
+import CastlesApi from './CastlesApi.js';
 
 
 class SearchList extends Component{
 
 
+// test from myBooks reader
+  state = {
+    query: '',
+    results: []
+  }
 
+
+  updateQuery = (query) => {
+    this.setState({
+      query: query
+    })
+    this.updateResults(query);
+  }
+
+
+  updateResults = (query) => {
+
+    if (query) {
+      Castle.then((results) => {
+        //error handling ternary for search
+        results.error ? this.setState({ results: [] }) : this.setState({ results: results })
+      })
+    } else {
+      this.setState({ results: '[]' });
+    }
+  }
+
+
+
+  //
+  // if (query) {
+  //   CastlesAPI.search(query).then((results) => {
+  //     //error handling ternary for search
+  //     results.error ? this.setState({ results: [] }) : this.setState({ results: results })
+  //   })
+  // } else {
+  //   this.setState({ results: [] });
+  // }
+  // }
 
 
 render() {
   return (
+
+
+
+
+
+
     <div className="castles-list">
+    <input
+      type="text"
+      placeholder="Search a castle"
+      value={this.state.query}
+      onChange={(event) => this.updateQuery(event.target.value)}
+    />
 
-<ol>
+
+    <ol>
                 {
+                   this.state.results.map(result => {
+                     let shelfName = "none";
 
-                      <Castle />
+                     this.props.list.map(castle => (
+                       castle.id === result.id ?
+                       'listName = castle.list' :
+                       ''
+                     ));
 
-                }
-                </ol>
+                     return (
+                     <li key={result.id}>
+                     <Castle
+                     castle={result}
+                     />
 
-
+                     </li>
+                   );
+                   })
+                 }
+</ol>
 
 
 <img className="pb" src={ require('./img/dracula-castles.png') } />
