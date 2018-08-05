ddb235538cff6a49476a67ed19dc40c2ad853864
Ewa-Stasiak <ewa.stasiak.poczta@gmail.com>
2018-08-05 11:36:24 +0200

    fix: Photo array display

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -54,8 +54,7 @@ class MapContainer extends Component {
         >
 
 
-{// Create markers from JSON locations
-}
+
 {Castles.map((castle) => {
 
   return ( <Marker
@@ -77,29 +76,31 @@ class MapContainer extends Component {
 
                       marker={this.props.activeMarker}
 
-                      visible={this.props.showingInfoWindow}
-          >
-          {
+                      visible={this.props.showingInfoWindow}>
+
+          {Castles.map((castle) => {
+            return (
                       <div>
 {this.props.fetchedPics}
                         <h2>{this.props.selectedPlace.title}</h2>
 
-                        <p>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+                        <p>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.
 
 </p>
                       </div>
 
-                    }
-          </InfoWindow>
 
 
+)
+})}
+</InfoWindow>
 
       </Map>
 
-    );
-  }
 
 
+
+)
 }
 
 export default GoogleApiWrapper({
