277848354b9a5af684b09dad6f32b7e675abe92d
Ewa-Stasiak <ewa.stasiak.poczta@gmail.com>
2018-08-05 19:04:36 +0200

    fix: Infowindow issues

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -91,10 +91,10 @@ class MapContainer extends Component {
               return (
 
                       <div className={"info"}>
-                      {this.props.fetchedPics}
+                      {this.props.fetchedPics[castle.name]}
 
                         <h2 className={"info"}>{this.props.selectedPlace.title}</h2>
-
+                        <a href={castle.wikiLink} target="_blank">Read more</a>
                         <p className={"info"}>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
                         </p>
                       </div>
